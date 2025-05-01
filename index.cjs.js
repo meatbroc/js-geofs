da21fee82ca21c744ad5764bbf17a756b7725e7a
meatbroc <168386225+meatbroc@users.noreply.github.com>
2025-05-01 13:33:07 -0400

    Add files via upload

---
+++ index.cjs.js
@@ -0,0 +1,7 @@
+const { MapAPI } = require('./lib/mapAPI.cjs.js');
+const { MultiplayerAPI } = require('./lib/multiplayerAPI.cjs.js');
+
+module.exports = {
+    MapAPI,
+    MultiplayerAPI
+};
---
+++ index.mjs
@@ -0,0 +1,2 @@
+export { MapAPI } from "./lib/mapAPI.mjs";
+export { MultiplayerAPI } from "./lib/multiplayerAPI.mjs";
