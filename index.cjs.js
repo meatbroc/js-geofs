18279e5a4f6a4540405496f29f69c9716b4db740
meatbroc <168386225+meatbroc@users.noreply.github.com>
2025-05-02 13:48:39 -0400

    added weatherAPI

--- index.cjs.js
+++ index.cjs.js
@@ -1,9 +1,11 @@
 const { MapAPI } = require('./lib/mapAPI.cjs.js');
 const { MultiplayerAPI } = require('./lib/multiplayerAPI.cjs.js');
 const { GeoAPI } = require('./lib/geoAPI.cjs.js');
+const { WeatherAPI } = require('./lib/weatherAPI.cjs.js');
 
 module.exports = {
     MapAPI,
     MultiplayerAPI,
-    GeoAPI
+    GeoAPI,
+    WeatherAPI,
 };
--- index.mjs
+++ index.mjs
@@ -1,3 +1,4 @@
 export { MapAPI } from "./lib/mapAPI.mjs";
 export { MultiplayerAPI } from "./lib/multiplayerAPI.mjs";
 export { GeoAPI } from "./lib/geoAPI.mjs";
+export { WeatherAPI } from "./lib/weatherAPI.mjs";
