bc141cae7600cc34b0a984348083249a68e4b1a1
meatbroc <168386225+meatbroc@users.noreply.github.com>
2025-05-02 12:35:49 -0400

    added geoAPI

--- index.cjs.js
+++ index.cjs.js
@@ -1,7 +1,9 @@
 const { MapAPI } = require('./lib/mapAPI.cjs.js');
 const { MultiplayerAPI } = require('./lib/multiplayerAPI.cjs.js');
+const { GeoAPI } = require('./lib/geoAPI.cjs.js');
 
 module.exports = {
     MapAPI,
-    MultiplayerAPI
+    MultiplayerAPI,
+    GeoAPI
 };
--- index.mjs
+++ index.mjs
@@ -1,2 +1,3 @@
 export { MapAPI } from "./lib/mapAPI.mjs";
 export { MultiplayerAPI } from "./lib/multiplayerAPI.mjs";
+export { GeoAPI } from "./lib/geoAPI.mjs";
